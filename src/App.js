ea94ceabb401f08ab1eac56e965d71a17a18406f
GomuGagan <gagangomu@gmail.com>
2022-08-25 21:42:05 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./app.css"
+import TicTacToe from './TicTacToe';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <TicTacToe />
+    </div>
+  )
+}
+
+export default App;
